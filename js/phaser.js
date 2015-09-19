ecf3f6bc56861c50c0d55e12fb614f5d15021b73
Wayne Hutchinson <wfh2727@gmail.com>
2015-09-18 22:42:27 -0500

    Added firing and health for player and enemy.

--- js/phaser.js
+++ js/phaser.js
@@ -1,129 +1,19 @@
-// var game = new Phaser.Game(800, 600, Phaser.AUTO);
 
-// var player;
-// var score = 0;
-// var scoreText;
-// var grass;
-// var ground;
-// var cursors;
-// var cabinet;
-
-
-// var GameState = {
-//   preload: function() {
-//     game.load.image('sky', 'assets/sky3.png');
-//     game.load.image('grass', 'assets/light_grass.png');
-//     game.load.image('ground', 'assets/ground-tile.png');
-//     game.load.image('cabinet', 'assets/file-cabinet.png');
-//     game.load.image('beet', 'assets/beet.png');
-//     game.load.image('cloud', 'assets/cloud.png');
-//     game.load.spritesheet('bear', 'assets/bear.png', 32, 32);
-//     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-//   },
-
-//   create: function() {
-
-//     game.physics.startSystem(Phaser.Physics.ARCADE);
-
-//     game.sky = game.add.tileSprite(0, 0, 800, 600, 'sky');
-//     grass = game.add.tileSprite(0, 550, 800, 50, 'grass');
-//     // grass.enableBody(true);
-//     // grass.body.immovable = true;
-
-//     ground = game.add.tileSprite(0, 550, 800, 50, 'ground');
-//     // ground.enableBody(true);
-//     // ground.body.immovable = true;
-
-//     game.cloud = game.add.sprite(20, 20, 'cloud');
-
-//     cabinet = game.add.sprite(200, 475, 'cabinet');
-//     cabinet.scale.setTo(2.5);
-
-//     game.beet = game.add.sprite(300, 510, 'beet');
-//     game.beet.scale.setTo(1.5);
-
-//     game.bear = game.add.sprite(600, 520, 'bear');
-//     game.bear.anchor.setTo(0.5);
-//     game.bear.scale.setTo(4);
-
-//     game.physics.arcade.enable(game.bear);
-
-//     game.bear.animations.add('move', [0, 0, 0, 0, 1, 2, 2, 2, 2, 3], 4, true);
-
-//     game.bear.animations.play('move', 4, true);
-
-//     player = game.add.sprite(700, 400, 'dude');
-
-
-
-
-//     // The player and its settings
-//     // player = game.add.sprite(32, game.world.height - 150, 'dude');
-//     //  We need to enable physics on the player
-//     game.physics.arcade.enable(player);
-//     //  Player physics properties. Give the little guy a slight bounce.
-//     player.body.bounce.y = 0.2;
-//     player.body.gravity.y = 300;
-//     player.body.collideWorldBounds = true;
-//     //  Our two animations, walking left and right.
-//     player.animations.add('left', [0, 1, 2, 3], 10, true);
-//     player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-//     cursors = game.input.keyboard.createCursorKeys();
-
-//   },
-
-//   update: function() {
-
-//     game.physics.arcade.collide(player, grass);
-//     game.physics.arcade.collide(player, cabinet);
-
-
-//     player.body.velocity.x = 0;
-//     if (cursors.left.isDown)
-//     {
-//         //  Move to the left
-//         player.body.velocity.x = -150;
-//         player.animations.play('left');
-//     }
-//     else if (cursors.right.isDown)
-//     {
-//         //  Move to the right
-//         player.body.velocity.x = 150;
-//         player.animations.play('right');
-//     }
-//     else
-//     {
-//         //  Stand still
-//         player.animations.stop();
-//         player.frame = 4;
-//     }
-    
-//     //  Allow the player to jump if they are touching the ground.
-//     if (cursors.up.isDown)
-//     {
-//         player.body.velocity.y = -350;
-//     }
-//   }
-// };
-
-// game.state.add("GameState", GameState);
-// game.state.start("GameState");
-
-
-
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render:render });
 
 function preload() {
     game.load.image('sky', 'assets/sky3.png');
     game.load.image('cloud', 'assets/cloud.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('beet', 'assets/beet.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.spritesheet('dude', 'assets/new-dude.png', 32, 48);
     game.load.image('cabinet', 'assets/file-cabinet.png');
     game.load.spritesheet('bear', 'assets/bear.png', 32, 32);
-    game.load.image('ship', 'assets/battlestar.png');
+    game.load.image('enemy', 'assets/battlestar.png');
     game.load.image('grounds', 'assets/ground-tile.png');
+    game.load.image('enemy', 'assets/enemy-bullet.png');
+    game.load.spritesheet('explosion1', 'assets/boom32wh12.png', 32, 32);
+    game.load.spritesheet('explosion2', 'assets/explosion.png', 64, 64);
 }
 
 var player;
@@ -132,12 +22,22 @@ var cursors;
 var beets;
 var score = 0;
 var scoreText;
+var enemyText;
+var playerText;
 var sky;
 var cloud;
 var cabinet;
 var bear;
-var ship;
+var bear2;
+var enemy;
 var grounds;
+var bullets;
+var enemyBullets;
+var bulletTime = 0;
+var enemyHit;
+var enemyHealth = 100;
+var playerHealth = 100;
+var firingTimer = 0;
 
 function create() {
     //  We're going to be using physics, so enable the Arcade Physics system
@@ -150,10 +50,34 @@ function create() {
     cloud = game.add.sprite(500, 90, 'cloud');
     cloud = game.add.sprite(550, 60, 'cloud');
 
-    ship = game.add.sprite(29, 70, 'ship');
-    ship.scale.setTo(4);
-    ship.enableBody = true;
-    game.physics.arcade.enable(ship);
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(30, 'beet');
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 0.5);
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true);
+
+    // The enemy's bullets
+    enemyBullets = game.add.group();
+    enemyBullets.enableBody = true;
+    enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
+    enemyBullets.createMultiple(30, 'enemy');
+    enemyBullets.setAll('anchor.x', 0.5);
+    enemyBullets.setAll('anchor.y', 1);
+    enemyBullets.setAll('outOfBoundsKill', true);
+    enemyBullets.setAll('checkWorldBounds', true);
+    // enemyBullets.scale.setTo(2);
+
+
+
+    enemy = game.add.sprite(100, 90, 'enemy');
+    enemy.scale.setTo(4);
+    enemy.enableBody = true;
+    game.physics.arcade.enable(enemy);
+    enemy.anchor.setTo(0.5, 0.5);
+    enemy.body.setSize(20, 10, 0, 20);
 
 
     //  The platforms group contains the ground
@@ -181,22 +105,30 @@ function create() {
         cab.anchor.setTo(0.5);
         game.physics.arcade.enable(cab);
         cab.body.immovable = true;
+        cab.body.setSize(15, 22, 0, -3);
     }
 
-    game.bear = game.add.sprite(Math.random() * 700, 503, 'bear');
-    game.bear.anchor.setTo(0.5);
-    game.bear.scale.setTo(4);
-    game.bear.enableBody = true;
+    bear = game.add.sprite(Math.random() * 700, 503, 'bear');
+    bear.anchor.setTo(0.5);
+    bear.scale.setTo(4);
+    bear.enableBody = true;
+    game.physics.arcade.enable(bear);
+    bear.body.setSize(23, 15, -15, 0);
+    bear.animations.add('move', [0, 0, 0, 0, 0, 1, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2], 5, true);
+    bear.animations.play('move', 4, true);
 
-    game.physics.arcade.enable(game.bear);
-
-    game.bear.animations.add('move', [0, 0, 0, 0, 0, 1, 1, 2, 3, 3, 3, 3, 2], 5, true);
-
-    game.bear.animations.play('move', 4, true);
-
+    bear2 = game.add.sprite(Math.random() * 700, 503, 'bear');
+    bear2.anchor.setTo(0.5);
+    bear2.scale.setTo(4);
+    bear2.enableBody = true;
+    game.physics.arcade.enable(bear2);
+    bear2.body.setSize(23, 15, -15, 0);
+    bear2.animations.add('move', [0, 0, 0, 1, 1, 2, 3, 3, 3, 2], 3, true);
+    bear2.animations.play('move', 4, true);
 
     // The player and its settings
     player = game.add.sprite(32, game.world.height - 150, 'dude');
+    player.anchor.setTo(0.5);
     player.scale.setTo(1.5);
     //  We need to enable physics on the player
     game.physics.arcade.enable(player);
@@ -222,35 +154,40 @@ function create() {
         beet.body.bounce.y = 0.3 + Math.random() * 0.2;
     }
     //  The score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+    enemyText = game.add.text(250, 16, 'Enemy Health: 100%', { fontSize: '32px', fill: '#000' });
+    playerText = game.add.text(590, 16, 'Health: 100%', { fontSize: '32px', fill: '#000' });
     //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
+    fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     
 }
 
 function update() {
 
-    if (game.bear.position.x < 50) {
-        game.bear.position.x = 200;
-    }
-
-    // Move ship back and forth
-    if (ship.body.position.x < 30) {
-        game.add.tween(ship).to({x: 661}, 5000, Phaser.Easing.Out, true);
+    // Move enemy back and forth
+    if (enemy.body.position.x < 70) {
+        game.add.tween(enemy).to({x: 561}, 5000, Phaser.Easing.Out, true);
     }
     
-    if (ship.body.position.x > 660) {
-        game.add.tween(ship).to({x: 29}, 5000, Phaser.Easing.Out, true);
+    if (enemy.body.position.x > 520) {
+        game.add.tween(enemy).to({x: 69}, 5000, Phaser.Easing.Out, true);
     }
 
-    //  Collide the player and the beets with the platforms
+    //  Collide the player and the beets with the platforms and cabinets
     game.physics.arcade.collide(player, platforms);
     game.physics.arcade.collide(player, cabinet);
     game.physics.arcade.collide(beets, platforms);
     game.physics.arcade.collide(beets, cabinet);
-    //  Checks to see if the player overlaps with any of the beets, if he does call the collectBeet function
+    //  Checks to see if the player overlaps with any of the beets or bear, if he does call the collectBeet function
     game.physics.arcade.overlap(player, beets, collectBeet, null, this);
-    game.physics.arcade.overlap(player, game.bear, hitBear, null, this);
+
+    game.physics.arcade.overlap(player, bear, hitBear, null, this);
+    game.physics.arcade.overlap(player, bear2, hitBear, null, this);
+
+    game.physics.arcade.overlap(bullets, enemy, collisionHandler, null, this);
+    game.physics.arcade.overlap(enemyBullets, player, playerHit, null, this);
+   
     //  Reset the players velocity (movement)
     player.body.velocity.x = 0;
     if (cursors.left.isDown)
@@ -277,17 +214,117 @@ function update() {
     {
         player.body.velocity.y = -350;
     }
+
+    //  Firing?
+    if (fireButton.isDown)
+    {
+        fireBullet();
+    }
+
+    if (game.time.now > firingTimer)
+    {
+        enemyFires();
+    }
 }
+
+function collisionHandler (enemy, bullet) {
+    bullet.kill();
+    score += 50;
+    scoreText.text = 'Score: ' + score;
+    enemyHealth -= 5;
+    enemyText.text = 'Enemy Health: ' + enemyHealth + '%';
+    if (enemyHealth === 0) {
+        enemy.kill();
+        enemyBullets.callAll('kill');
+        enemy.alive = false;
+        player.alive = false;
+    }
+
+    var explosion = this.add.sprite(enemy.x, enemy.y, 'explosion2');
+    explosion.anchor.setTo(0.5, 0.5);
+    explosion.animations.add('boom');
+    explosion.play('boom', 15, false, true);
+
+}
+
+function fireBullet () {
+    if (player.alive === true) {
+        //  To avoid them being allowed to fire too fast we set a time limit
+        if (game.time.now > bulletTime)
+        {
+            //  Grab the first bullet we can from the pool
+            bullet = bullets.getFirstExists(false);
+
+            if (bullet)
+            {
+                //  And fire it
+                bullet.reset(player.x, player.y + 8);
+                bullet.body.velocity.y = -400;
+                bulletTime = game.time.now + 200;
+            }
+        }
+    }
+}
+
+function enemyFires () {
+
+    if (enemy.alive === true) {
+
+    //  Grab the first bullet we can from the pool
+    enemyBullet = enemyBullets.getFirstExists(false);
+
+    var shooter = enemy;
+        // And fire the bullet from this enemy
+    enemyBullet.reset(enemy.body.x, enemy.body.y);
+
+    game.physics.arcade.moveToObject(enemyBullet,player,200);
+    firingTimer = game.time.now + 1500;
+
+    }
+}
+
+function playerHit (player, bullet) {
+    bullet.kill();
+    score -= 50;
+    scoreText.text = 'Score: ' + score;
+    playerHealth -= 10;
+    playerText.text = 'Health: ' + playerHealth + '%';
+
+    var explosion = this.add.sprite(player.x, player.y, 'explosion1');
+    explosion.anchor.setTo(0.5, 0.5);
+    explosion.animations.add('boom');
+    explosion.play('boom', 15, false, true);
+
+    if (playerHealth <= 0) {
+        player.kill();
+        bullets.callAll('kill');
+        player.alive = false;
+        enemy.alive = false;
+    }
+}
+
 function collectBeet (player, beet) {
     // Removes the beet from the screen
     beet.kill();
     //  Add and update the score
     score += 100;
     scoreText.text = 'Score: ' + score;
+    playerHealth += 2;
+    playerText.text = 'Health: ' + playerHealth + '%';
+
 }
 
 //  Lose points for running into the bear
 function hitBear () {
     score -= 1;
     scoreText.text = 'Score: ' + score;
+    // playerHealth = playerHealth - 10;
+    // playerText.text = 'Health: ' + playerHealth + '%';
+}
+
+function render() {
+    // game.debug.body(enemy);
+    // cabinet.forEach(function(cab) {
+    //     game.debug.body(cab);
+    // })
 }
